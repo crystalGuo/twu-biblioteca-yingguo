d0629fa6f2c9f22d53cf1d4152a22a4253c3c6bb
crystalGuo <793832732@qq.com>
2017-01-09 00:56:51 +0800

    #Story-1 show welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,38 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 
 public class ExampleTest {
 
-    @Test
-    public void test() {
-        assertEquals(1, 1);
+    private ByteArrayOutputStream output;
+
+    @Before
+    public void setUpStreams() throws Exception {
+        output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
     }
+
+    @Test
+    public void testWelcomMessage() {
+        String[] args = {};
+        BibliotecaApp.main(args);
+
+        assertThat(output.toString(), is("Welcome to Bibliteca!\n"));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
 }
